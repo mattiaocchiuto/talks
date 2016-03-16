f53f9de99b7568ae7023b4bead091f0539cc77d3
mattia <occhiutomattia@gmail.com>
2016-03-16 09:57:50 +0100

    aggiunto esempio creazione Observable

---
+++ RxJS/src/wrap.js
@@ -0,0 +1,34 @@
+var Rx = require('rx');
+
+var source = Rx.Observable.create(function (observer) {
+  var id = setTimeout(function () {
+    console.log('started');
+    try {
+      throw 'error';
+      observer.onNext(34);
+      observer.onCompleted();
+    } catch (error) {
+      observer.onError(error);
+    }
+
+  }, 1000);
+
+  return function dispose() {
+    console.log('disposed');
+    clearTimeout(id);
+  };
+});
+
+var subscription = source.subscribe(
+  function (x) {
+    console.log('Next: ' + x);
+  }, function (e) {
+    console.log('Error: ' + e);
+  }, function () {
+    console.log('Completed');
+  }
+);
+
+/*setTimeout(function () {
+  subscription.dispose();
+}, 500);*/
