d659085532d93b095043512ae383b9009c7ee089
mattia <occhiutomattia@gmail.com>
2016-03-18 14:05:25 +0100

    aggiunta gestione online/offline

--- RxJS/src/autocomplete.js
+++ RxJS/src/autocomplete.js
@@ -18,12 +18,13 @@
   function main() {
     var $input = $('#textInput');
     var $results = $('#results');
+    var $networkState = $('.page-header h1');
 
     var keyup = Rx.Observable.fromEvent($input, 'keyup')
-      .map(function (e) {
+      .map((e) => {
         return e.target.value;
       })
-      .filter(function (text) {
+      .filter((text) => {
         return text.length > 2;
       })
       // Passa il valore se tra un valore e l'altro è presente una pausa di almeno 750 ms
@@ -31,26 +32,40 @@
       // Solo se il valore è cambiato
       .distinctUntilChanged();
 
+    var networkState = Rx.Observable
+      .merge(
+        Rx.Observable.fromEvent(window, 'online').map(() => 1),
+        Rx.Observable.fromEvent(window, 'offline').map(() => 0)
+      )
+      .startWith(navigator.onLine ? 1 : 0);
+
     var searcher = keyup
       // Come flatMap ma non appena l'observable passato alla flat ritorna un nuovo
       // observable si viene disiscritti da quello precedente.
       .flatMapLatest((x) => {
         return Rx.Observable.fromPromise(searchWikipedia(x))
-          //.retry(3)
           .retryWhen((error) => {
             return navigator.onLine ?
               Rx.Observable.timer(3000) :
-              Rx.Observable.fromEvent(window, 'online').take(1);
+              networkState;
           });
       });
 
+    networkState.subscribe(
+      (state) => {
+        $networkState
+          .removeClass()
+          .addClass(state ? 'online' : 'offline');
+      }
+    );
+
     searcher.subscribe(
-      function (data) {
+      (data) => {
         $results
           .empty()
           .append ($.map(data[1], function (v) { return $('<li>').text(v); }));
       },
-      function (error) {
+      (error) => {
         $results
           .empty()
           .append($('<li>'))
