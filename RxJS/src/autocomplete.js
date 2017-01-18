116219d48766987f4eb91620fcd5997a92cfdec8
mattia <occhiutomattia@gmail.com>
2017-01-18 18:17:22 +0100

    Fix autocomplete example

--- RxJS/src/autocomplete.js
+++ RxJS/src/autocomplete.js
@@ -29,7 +29,7 @@
     const search$ = inputKeyup$
       .map(e => e.target.value)
       .filter(text => text.length > 2)
-      .debounceTime(200)
+      .debounceTime(500)
       .distinctUntilChanged();
 
     const networkOn$ = Rx.Observable.fromEvent(scope, NETWORK_ON).mapTo(true);
@@ -46,7 +46,7 @@
     const searchResult$ = search$
       .switchMap(term =>
         Rx.Observable.defer(() => searchWikipedia(term))
-          .retryWhen(error => navigator.onLine ? Rx.Observable.debounceTime(5000) : networkState$)
+          .retryWhen(error => navigator.onLine ? Rx.Observable.debounce(5000) : networkState$)
       );
 
     // ***********************
